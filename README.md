2862c4e83b63d7c32a84591b147e1c00ba49d353
tamwibu <168276070+tamwibu@users.noreply.github.com>
2024-04-28 08:27:05 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# web-haiten-sword-
---
+++ tam java.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// https://manuel.pinto.dev
+function triggerBtnClick()
+{
+	document.getElementById('btn').click();
+}
+const cursor = document.querySelector(".cursor");
+var timeout;
+
+//follow cursor on mousemove
+document.addEventListener("mousemove", (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top = y + "px";
+    cursor.style.left = x + "px";
+    cursor.style.display = "block";
+
+    //cursor effects when mouse stopped
+    function mouseStopped() {
+        cursor.style.display = "none";
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(mouseStopped, 5000);
+});
+
+//cursor effects when mouseout
+document.addEventListener("mouseout", () => {
+    cursor.style.display = "none";
+});
